0f3139809458f880a4fe916f1959d8d7261c17f7
adilnaiyar <adil.naiyar@gmail.com>
2021-03-16 14:17:37 +0530

    created crud operation in project of users with dummy data in db.json

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Contact from './components/Pages/Contact.components';
 import './App.css';
 import Navbar from './components/Layout/Navbar.components';
 import NotFound from './components/Pages/NotFound.components';
+import AddUser from "./components/Users/AddUser.components";
+import EditUser from "./components/Users/EditUser.components";
+import ViewUser from "./components/Users/ViewUser.components";
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
+          <Route exact path="/users/add" component={AddUser} />
+          <Route exact path="/users/edit/:id" component={EditUser} />
+          <Route exact path="/users/:id" component={ViewUser} />
           <Route component={NotFound} />
       </Switch>
     </div>
